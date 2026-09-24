80109b48a7627e4dfd83590e493eebe2c9519e8d
miglitopictures <miguelmddesign@gmail.com>
2026-09-24 16:13:51 -0300

    dto para instituicoes

---
+++ backend/src/main/java/com/hemotrack/backend/model/instituicao/dto/InstituicaoRequest.java
@@ -0,0 +1,26 @@
+package com.hemotrack.backend.model.instituicao.dto;
+
+import com.hemotrack.backend.model.instituicao.TipoInstituicao;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record InstituicaoRequest(
+    @NotBlank (message = "Razao social é obrigatoria")
+    @Size (min = 3, max = 80, message = "Razão social deve ter no minimo 3 e no máximo 80 caractéres")
+    String razaoSocial,
+
+    @NotBlank (message = "CNPJ é obrigatoria")
+    String cnpj,
+
+    @NotBlank (message = "Tipo da instituição é obrigatoria")
+    TipoInstituicao tipo,
+
+    @NotBlank (message = "Endereço é obrigatório")
+    String endereco,
+
+    @NotBlank (message = "Município é obrigatório")
+    String municipio,
+
+    String telefone
+) { }
