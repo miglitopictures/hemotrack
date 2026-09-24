81d70deb17d08128ff1a54240ba97664be4e9b78
miglitopictures <miguelmddesign@gmail.com>
2026-09-24 16:02:54 -0300

    implementando Repositories para Instituicao e Usuario (Cadastro e login)

--- backend/src/main/java/com/hemotrack/backend/repositories/InstituicaoRepository.java
+++ backend/src/main/java/com/hemotrack/backend/repositories/InstituicaoRepository.java
@@ -1,9 +1,13 @@
 package com.hemotrack.backend.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hemotrack.backend.model.instituicao.Instituicao;
 
 public interface InstituicaoRepository extends JpaRepository<Instituicao, Long>{
+    boolean existsByCnpj(String cnpj);
+    Optional<Instituicao> findByCnpj(String cnpj);
     
 }
--- backend/src/main/java/com/hemotrack/backend/repositories/UsuarioRepository.java
+++ backend/src/main/java/com/hemotrack/backend/repositories/UsuarioRepository.java
@@ -1,14 +1,14 @@
 package com.hemotrack.backend.repositories;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.hemotrack.backend.model.usuario.Usuario;
-// import org.springframework.stereotype.Repository;
 
 
-//@Repository (inutil aparentemente, pois quando estamos usado JpaRepository)
-
+// em todas essas classes que extendem JpaRepository, nos na temos que implementar "nada". A partir do nome do método (ex. findByEmail, findById), o Spring Data vai gerar o query SQL em tempo de execucao.
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
     
+    Optional<Usuario> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
